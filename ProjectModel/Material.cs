4d91e62ce9a0965f52f5b085c96d8e397a324847
Vicente Errázuriz <verrazuriz@uc.cl>
2019-09-28 21:21:52 -0300

    Refactor + Documentación

--- ProjectModel/Material.cs
+++ ProjectModel/Material.cs
@@ -2,6 +2,9 @@
 
 namespace ProjectModel
 {
+    /// <summary>
+    /// Representa un material de construcción
+    /// </summary>
     [Serializable]
     public class Material
     {
@@ -24,16 +27,24 @@ namespace ProjectModel
         private string units;
 
         /// <summary>
+        /// Familia de elementos de construcción a la que corresponde (pared, ventana, tubería, etc)
+        /// </summary>        
+        public string Family { get => family; }
+        private string family;
+
+        /// <summary>
         /// Inicializa un nuevo material
         /// </summary>
         /// <param name="name">Nombre del material</param>
         /// <param name="price">Precio por unidad del material</param>
         /// <param name="units">Unidades en las que se mide la cantidad de material (Kg, m3, etc)</param>
-        public Material(string name, int price, string units)
+        /// <param name="family">Familia de elementos de construcción a la que corresponde (pared, ventana, tubería, etc)</param>
+        public Material(string name, int price, string units, string family)
         {
             this.name = name;
             this.price = price;
             this.units = units;
+            this.family = family;
         }
     }
 }
