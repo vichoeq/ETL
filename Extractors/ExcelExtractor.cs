0bc9f78a4e9abdefc9ad7cc4a337a842959401c4
fipaniagua <fipaniagua@uc.cl>
2019-09-30 22:46:54 -0300

    current problem in taskextractor

--- Extractors/ExcelExtractor.cs
+++ Extractors/ExcelExtractor.cs
@@ -31,21 +31,22 @@ namespace Extractors
         {
             List<Material> rows_info = new List<Material>();
 
-            for (int i = 1; i <= rowCount; i++)
+            for (int i = 4; i <= rowCount; i++)
             {
                 if (XlRange.Cells[i, 2] != null && XlRange.Cells[i, 2].Value2 != null)
                 {
+                    //Console.WriteLine("row:" + i.ToString());
                     //the data is preprocess by a dictionary so the parser is agnostic to the order of the columns
                     Dictionary<string, string> row_info = new Dictionary<string, string>();
                     for (int j = 1; j <= colCount; j++)
                     {
                         if (XlRange.Cells[i, j] != null && XlRange.Cells[i, j].Value2 != null)
                         {
+                            //Console.WriteLine(XlRange.Cells[3, j].Value2.ToString() + "/" + XlRange.Cells[i, j].Value2.ToString());
                             row_info[XlRange.Cells[3, j].Value2.ToString()] = XlRange.Cells[i, j].Value2.ToString();
                         }
                     }
-                    Material newMaterial = new Material(row_info["Tipo de familia"], Convert.ToInt32(row_info["Precio Unitario del item"]),
-                                                        row_info["Unidad"], row_info["Familia"]);
+                    Material newMaterial = new Material(row_info["Tipo de familia"], Convert.ToInt32(row_info["Precio Unitario del item"]),row_info["Unidad"], row_info["Familia"]);
                     rows_info.Add(newMaterial);
                 }                
             }
---
+++ testing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Extractors;
+using ProjectModel;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathExcel = @"C:\Users\Usuario\Documents\archibos proyecto capstone\Estimación de costos.xlsm";
+            string pathProject = @"C:\Users\Usuario\Documents\archibos proyecto capstone\Modelo de proceso.mpp";
+            ExcelExtractor excelExtractor = new ExcelExtractor(pathExcel);
+            MicrosoftProjectFile projectFileExtractor = new MicrosoftProjectFile(pathProject);
+            List<Material> materials = excelExtractor.Extract();
+            Dictionary<(string, Phase, int), List<Task>> tasks = projectFileExtractor.Extract();
+            Console.WriteLine("Materials:");
+            foreach (Material material in materials)
+            {
+                Console.WriteLine(material.Name);
+            }
+            Console.WriteLine("Task:");
+            foreach (KeyValuePair<(string, Phase, int), List<Task>> entry in tasks)
+            {
+                Console.WriteLine(entry.Key.Item1 + "/" + entry.Key.Item2.ToString() + "/" + entry.Key.Item3.ToString() + ":" );
+                foreach (Task task in entry.Value)
+                {
+                    Console.WriteLine(task.Name);
+                }
+                // do something with entry.Value or entry.Key
+            }
+
+            var key = Console.ReadKey();
+
+
+        }
+    }
+}
